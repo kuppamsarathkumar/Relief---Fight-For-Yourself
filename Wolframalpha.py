0ce7bbcc361242b041fd552b2b5d55c5d88f250c
kuppamsarathkumar <107363946+kuppamsarathkumar@users.noreply.github.com>
2023-06-17 19:59:27 +0530

    Delete Wolframalpha.py

--- Wolframalpha.py
+++
@@ -1,7 +0,0 @@
-import wolframalpha
-client=wolframalpha.Client('64TPRH-Y5JR5WRJ89')
-while True:
-    query=str(input("Query : "))
-    res=client.query(query)
-    output= next(res.results).text
-    print(output)
